4800d634818968ecc253312969b0f76efe240866
Andrei <andrei8246@gmail.com>
2024-10-02 22:30:10 -0300

    subindo

---
+++ src/main/java/com/andreirpc/client/Main.java
@@ -0,0 +1,7 @@
+package com.andreirpc.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("'");
+    }
+}
---
+++ src/main/java/com/andreirpc/client/RPCClient.java
@@ -0,0 +1,68 @@
+package com.andreirpc.client;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.*;
+
+public class RPCClient implements AutoCloseable {
+
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+
+    public RPCClient() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+    }
+
+    public static void main(String[] argv) {
+        try (RPCClient rpcClient = new RPCClient()) {
+            // Enviando o nome "Ândrei" para o RPCServer
+            String name = "Ândrei de Medeiros Lucena";
+            System.out.println(" Enviando o nome: " + name);
+            String response = rpcClient.call(name);
+            System.out.println(" [.] Got '" + response + "'");
+        } catch (IOException | TimeoutException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String call(String message) throws IOException, InterruptedException, ExecutionException {
+        final String corrId = UUID.randomUUID().toString();
+
+        String replyQueueName = channel.queueDeclare().getQueue();
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+
+        // Enviando o nome "Ândrei" ao invés de um número
+        channel.basicPublish("", requestQueueName, props, message.getBytes("UTF-8"));
+
+        final CompletableFuture<String> response = new CompletableFuture<>();
+
+        String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response.complete(new String(delivery.getBody(), "UTF-8"));
+            }
+        }, consumerTag -> {
+        });
+
+        String result = response.get();
+        channel.basicCancel(ctag);
+        return result;
+    }
+
+    public void close() throws IOException {
+        connection.close();
+    }
+}
